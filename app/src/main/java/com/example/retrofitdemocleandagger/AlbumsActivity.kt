610fbc96793b3172eb72beef805d69cef7ee2570
Erick Savluchinske <ericksav.dev@gmail.com>
2023-02-28 19:23:34 -0300

    project setup

--- app/src/main/java/com/example/retrofitdemocleandagger/AlbumsActivity.kt
+++ app/src/main/java/com/example/retrofitdemocleandagger/AlbumsActivity.kt
@@ -3,9 +3,9 @@ package com.example.retrofitdemocleandagger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AlbumsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_albums)
     }
 }
